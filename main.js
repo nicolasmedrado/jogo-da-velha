affc1ee5f62540fae328ba0ee02e3be7e37e1d7a
nicolasmedrado <nicozaum@gmail.com>
2025-09-16 16:13:04 -0400

    feat: game first full functioning version

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+let turn = 0
+let playerOneScore = 0
+let playerTwoScore = 0
+let playerOneShowScore = document.getElementById('player-one-score')
+let playerTwoShowScore = document.getElementById('player-two-score')
 const startBtn = document.getElementById('start-btn')
 const resetBtn = document.getElementById('reset-table')
 const menuBtn = document.getElementById('reset-game')
 const playerOne = document.getElementById('player1')
 const playerTwo = document.getElementById('player2')
 const playerScreen = document.getElementById('players')
-const gameScreen = document.getElementById('table')
+const gameTable = document.getElementById('table')
+const gameScreen = document.getElementById('game-screen')
+const playerScore = document.getElementById('score')
 const winnerScreen = document.getElementById('winner-screen')
+const winnerHeader = document.getElementById('game-result')
 const cells = document.querySelectorAll('[data-cell]')
-let turn = 0
-let playerOneScore = 0
-let playerTwoScore = 0
 const winningSequences = [
     [0, 1, 2],
     [3, 4, 5],
@@ -24,27 +29,57 @@ const winningSequences = [
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
     
-    const playerOneName = playerOne.value
-    const playerTwoName = playerTwo.value
+    document.getElementById('player-one-name').innerText = playerOne.value + ': '
+    document.getElementById('player-two-name').innerText = playerTwo.value + ': '
 
-    if (playerOneName === '' || playerTwoName === '') {
+    playerOneShowScore.innerText = '0'
+    playerTwoShowScore.innerText = '0'
+
+    if (playerOne === '' || playerTwo === '') {
         alert('Preencha os nomes!')
     } else {
+        playerOne.value = ''
+        playerTwo.value = ''
+        playerScore.style.display = 'flex'
         playerScreen.style.display = 'none'
-        gameScreen.style.display = 'grid'
+        gameScreen.style.display = 'flex'
+        gameScreen.style.flexDirection = 'column'
+        gameTable.style.display = 'grid'
     }
 })
 
-resetBtn.addEventListener('click', (ev) => {
-    ev.preventDefault()
+resetBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    playerScreen.style.display = 'none'
+    playerScore.style.display = 'flex'
+    winnerScreen.style.display = 'none'
+    gameScreen.style.display = 'flex'
+    gameTable.style.display = 'grid'
+    clearTable()
 })
 
-menuBtn.addEventListener('click', (ev) => {
-    ev.preventDefault()
+menuBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    playerScreen.style.display = 'flex'
+    winnerScreen.style.display = 'none'
+    gameTable.style.display = 'none'
+    gameScreen.style.display = 'none'
+    playerOneScore = 0
+    playerTwoScore = 0
+    playerOneShowScore.innerText = '0'
+    playerTwoShowScore.innerText = '0'
+    clearTable()
 })
 
 function currentPlayer() {
     return turn % 2 === 0 ? 'X' : 'O'
+}
+
+function clearTable() {
+    turn = 0
+    cells.forEach(cell => {
+        cell.textContent = ''
+    })
 }
 
 function checkWinner() {
@@ -57,20 +92,29 @@ function checkWinner() {
             cells[a].textContent === cells[c].textContent
         ) {
             playerScreen.style.display = 'none'
-            gameScreen.style.display = 'none'
+            gameTable.style.display = 'none'
             winnerScreen.style.display = 'flex'
+            playerScore.style.display = 'none'
             const winnerHeader = document.getElementById('game-result')
             winnerHeader.textContent = cells[a].textContent + ' venceu!'
+            if (cells[a].textContent === 'X') {
+                playerOneScore += 1
+                playerOneShowScore.innerText = ' ' + playerOneScore
+            } else if (cells[a].textContent === 'O') {
+                playerTwoScore += 1
+                playerTwoShowScore.innerText = ' ' + playerTwoScore
+            }
             return
         }
     }
 
     if ([...cells].every(cell => cell.textContent !== '')) {
             playerScreen.style.display = 'none'
+            gameTable.style.display = 'none'
             gameScreen.style.display = 'none'
             winnerScreen.style.display = 'flex'
-            const winnerHeader = document.getElementById('game-result')
             winnerHeader.textContent = 'Deu velha! :)'
+
     }
 
     return null
